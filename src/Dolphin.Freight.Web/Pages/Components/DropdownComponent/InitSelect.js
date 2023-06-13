5888c8ee0e4929b087f46db8473c5fcbdc87d1c2
Anand Parmar <anand.parmar@zobiwebsolutions.com>
2023-06-13 20:16:59 +0530

    DFREIGHT-188 DFREIGHT-191 Dynamic component created with search functionality.

---
+++ src/Dolphin.Freight.Web/Pages/Components/DropdownComponent/InitSelect.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    var l = abp.localization.getResource('Freight');
+
+});
+
+function getFormat() {
+
+    const optionFormat = (item) => {
+        if (!item.id) {
+            return item.text;
+        }
+
+        var span = document.createElement('span');
+        var template = '';
+
+        template += '<div class="d-flex align-items-center">';
+
+        template += '<div class="d-flex flex-column">'
+        template += '<span class="fs-6 lh-1">' + item.text + '</span>';
+        template += '</div>';
+        template += '</div>';
+
+        span.innerHTML = template;
+
+        return $(span);
+    }
+
+    return optionFormat;
+}
+
+function initializeDropdownSearch(id) {
+    $('#' + id).select2({
+        placeholder: "Select an option",
+        templateSelection: getFormat(),
+        templateResult: getFormat()
+    });
+}
+
+
+
