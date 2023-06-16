ebfc33c637eb85237ee55e8028e01bedf995f836
Anand Parmar <anand.parmar@zobiwebsolutions.com>
2023-06-16 17:19:16 +0530

    Ocean Export Function Menu JavaScript Functions Added.

--- src/Dolphin.Freight.Web/Pages/OceanExports/EditModal.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/OceanExports/EditModal.cshtml.cs
@@ -77,49 +77,49 @@ namespace Dolphin.Freight.Web.Pages.OceanExports
             QueryHblDto query = new QueryHblDto() { MblId = Id };
            OceanExportHbls = await _oceanExportHblAppService.QueryListByMidAsync(query);
             QueryHblDto queryHbl = new QueryHblDto();
-            //if (Hid == null)
-            //{
-            //    if (NewHbl == 1)
-            //    {
-            //        OceanExportHbl = new CreateUpdateOceanExportHblDto();
-            //        QueryDto cquery = new QueryDto();
-            //        cquery.QueryType = "CardColorId";
-            //        var syscodes = await _sysCodeAppService.GetSysCodeDtosByTypeAsync(cquery);
-            //        if (OceanExportHbls != null && OceanExportHbls.Count > 0)
-            //        {
-            //            int index =  OceanExportHbls.Count % syscodes.Count ;
-            //            OceanExportHbl.CardColorId = syscodes[index].Id;
-            //            OceanExportHbl.CardColorValue = syscodes[index].CodeValue;
-            //            CardClass = syscodes[index].CodeValue;
-            //        }
-            //        else 
-            //        {
-            //            OceanExportHbl.CardColorId = syscodes[0].Id;
-            //            OceanExportHbl.CardColorValue = syscodes[0].CodeValue;
-            //            CardClass = syscodes[0].CodeValue;
-            //        }
+            if (Hid == null)
+            {
+                if (NewHbl == 1)
+                {
+                    OceanExportHbl = new CreateUpdateOceanExportHblDto();
+                    QueryDto cquery = new QueryDto();
+                    cquery.QueryType = "CardColorId";
+                    var syscodes = await _sysCodeAppService.GetSysCodeDtosByTypeAsync(cquery);
+                    if (OceanExportHbls != null && OceanExportHbls.Count > 0)
+                    {
+                        int index = OceanExportHbls.Count % syscodes.Count;
+                        OceanExportHbl.CardColorId = syscodes[index].Id;
+                        OceanExportHbl.CardColorValue = syscodes[index].CodeValue;
+                        CardClass = syscodes[index].CodeValue;
+                    }
+                    else
+                    {
+                        OceanExportHbl.CardColorId = syscodes[0].Id;
+                        OceanExportHbl.CardColorValue = syscodes[0].CodeValue;
+                        CardClass = syscodes[0].CodeValue;
+                    }
 
-            //    }
-            //    else 
-            //    {
-            //        OceanExportHbl = new CreateUpdateOceanExportHblDto();
-            //        if (OceanExportHbls != null && OceanExportHbls.Count > 0)
-            //        {
-            //            OceanExportHbl = ObjectMapper.Map<OceanExportHblDto, CreateUpdateOceanExportHblDto>(OceanExportHbls[0]);
-            //            IsShowHbl = true;
-            //            ViewData["HAVEHBL"] = "Y";
-            //        }
-           //        
-            //    }
-            //
-            //}
-            //else {
+                }
+                else
+                {
+                    OceanExportHbl = new CreateUpdateOceanExportHblDto();
+                    if (OceanExportHbls != null && OceanExportHbls.Count > 0)
+                    {
+                        OceanExportHbl = ObjectMapper.Map<OceanExportHblDto, CreateUpdateOceanExportHblDto>(OceanExportHbls[0]);
+                        IsShowHbl = true;
+                        Hid = OceanExportHbl.Id;
+                        ViewData["HAVEHBL"] = "Y";
+                    }
+
+                }
+
+            }
+            else
+            {
                 queryHbl.Id = Hid;
                 OceanExportHbl = new();
-                IsShowHbl = true;
-
                 ViewData["HAVEHBL"] = "Y";
-            //}
+            }
             TempData["PrintData"] = JsonConvert.SerializeObject(OceanExportMbl);
         }
         public async Task<IActionResult> OnPostAsync()
