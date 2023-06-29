235428b64ea43ca8ae5567d748e2d126daee6e09
kenseco <kenseco@kooco.com.tw>
2023-06-30 03:33:16 +0800

    Fix dropdowns

--- src/Dolphin.Freight.Web/Pages/OceanImports/CreateMbl.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/OceanImports/CreateMbl.cshtml.cs
@@ -3,10 +3,14 @@ using Dolphin.Freight.Common;
 using Dolphin.Freight.ImportExport.OceanImports;
 using Dolphin.Freight.Settinngs.SysCodes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dolphin.Freight.TradePartners;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Linq;
+using Dolphin.Freight.Settings.PortsManagement;
 
 namespace Dolphin.Freight.Web.Pages.OceanImports
 {
@@ -17,23 +21,35 @@ namespace Dolphin.Freight.Web.Pages.OceanImports
         public Guid? Id { get; set; }
         [BindProperty]
         public CreateUpdateOceanImportMblDto? OceanImportMbl { get; set; }
+        public List<SelectListItem> TradePartnerLookupList { get; set; }
+        public List<SelectListItem> PortsManagementLookupList { get; set; }
+
         [BindProperty]
         public CreateUpdateOceanImportHblDto? OceanImportHbl { get; set; }
         [BindProperty]
         public int AddHbl { get; set; } = 0;
         private readonly IOceanImportHblAppService _oceanImportHblAppService;
         private readonly IOceanImportMblAppService _oceanImportMblAppService;
+        private readonly ITradePartnerAppService _tradePartnerAppService;
         private readonly ISysCodeAppService _sysCodeAppService;
-        public CreateMblModel( IOceanImportMblAppService oceanImportMblAppService,IOceanImportHblAppService oceanImportHblAppService, ISysCodeAppService sysCodeAppService)
+        private readonly IPortsManagementAppService _portsManagementAppService;
+
+        public CreateMblModel( IOceanImportMblAppService oceanImportMblAppService, IPortsManagementAppService portsManagementAppService, IOceanImportHblAppService oceanImportHblAppService, ISysCodeAppService sysCodeAppService, ITradePartnerAppService tradePartnerAppService)
         {
             _oceanImportMblAppService = oceanImportMblAppService;
             _oceanImportHblAppService = oceanImportHblAppService;
             _sysCodeAppService = sysCodeAppService;
+            _tradePartnerAppService = tradePartnerAppService;
+            _portsManagementAppService = portsManagementAppService;
+
         }
-        public void OnGet()
+        public async Task OnGetAsync()
         {
             OceanImportHbl = new CreateUpdateOceanImportHblDto();
             OceanImportMbl = new CreateUpdateOceanImportMblDto();
+
+            await FillTradePartnerAsync();
+            await FillPortAsync();
         }
         public async Task<JsonResult> OnPostAsync()
         {
@@ -66,6 +82,28 @@ namespace Dolphin.Freight.Web.Pages.OceanImports
                 { "id", Id.Value }
             };
             return new JsonResult(rs);
+
         }
+
+
+        #region FillTradePartnerAsync()
+        private async Task FillTradePartnerAsync()
+        {
+            var tradePartnerLookup = await _tradePartnerAppService.GetTradePartnersLookupAsync();
+            TradePartnerLookupList = tradePartnerLookup.Items
+                                                .Select(x => new SelectListItem(x.TPName + " / " + x.TPCode, x.Id.ToString(), false))
+                                                .ToList();
+        }
+        #endregion
+
+        #region FillPortAsync()
+        private async Task FillPortAsync()
+        {
+            var lookup = await _portsManagementAppService.QueryListAsync();
+
+            PortsManagementLookupList = lookup.Select(x => new SelectListItem(x.PortName, x.Id.ToString(), false))
+                                   .ToList();
+        }
+        #endregion
     }
 }
