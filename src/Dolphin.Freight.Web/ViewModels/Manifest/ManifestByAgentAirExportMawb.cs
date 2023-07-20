4cc77a43067ddcf17e7797012df3ad6b7cf784ab
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-07-20 12:00:37 +0530

    DFreight-529 Still need to Complete - Pending

---
+++ src/Dolphin.Freight.Web/ViewModels/Manifest/ManifestByAgentAirExportMawb.cs
@@ -0,0 +1,15 @@
+﻿using Org.BouncyCastle.Asn1.BC;
+using System.Collections.Generic;
+
+namespace Dolphin.Freight.Web.ViewModels.Manifest
+{
+    public class ManifestByAgentAirExportMawb
+    {
+        public List<OverSeaAgent> OverSeaAgents { get; set; }
+    }
+
+    public class OverSeaAgent
+    {
+        public string Name { get; set; }
+    }
+}
