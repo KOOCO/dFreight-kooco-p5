c667081c2dc2eb6762ed272c05413fa914cb7ef5
Pradeep <PrdpShrma13@users.noreply.github.com>
2023-07-26 18:02:35 +0530

    DFREIGHT-92 (Accounting Setting-Billing Code Manager list)

--- src/Dolphin.Freight.Web/Pages/AccountingSettings/BillingCodes/Index.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/AccountingSettings/BillingCodes/Index.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 
 namespace Dolphin.Freight.Web.Pages.AccountingSetting.BillingCodes
 {
     public class IndexModel : PageModel
     {
+        public string Code { get; set; }
+        [DisplayName("Name(Eng./Local)")]
+        public string Name { get; set; }
+        [DisplayName("G/L Code")]
+        public string GlCode { get; set; }
+        [DisplayName("G/L Description")]
+        public string GlDescription { get; set; }
         public void OnGet()
         {
         }
