89cc2d79839cf7ace27cdc918cf0fbb16ed3711f
Pradeep <PrdpShrma13@users.noreply.github.com>
2023-07-26 19:07:06 +0530

    Column IsPayroll added in BillingCode table

---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20230726133222_billing_code_Table_IsPayroll_column_Added.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class billing_code_Table_IsPayroll_column_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPayroll",
+                table: "AppBillingCodes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPayroll",
+                table: "AppBillingCodes");
+        }
+    }
+}
