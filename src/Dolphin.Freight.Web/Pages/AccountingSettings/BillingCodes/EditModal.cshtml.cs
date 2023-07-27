0b28d56e0503ca44b64536c664e59eadfe26d8db
Pradeep <PrdpShrma13@users.noreply.github.com>
2023-07-27 22:46:16 +0530

    Commit for DFREIGHT-998 (Accounting Setting-Billing Code Manager-dropdown in popup)

--- src/Dolphin.Freight.Web/Pages/AccountingSettings/BillingCodes/EditModal.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/AccountingSettings/BillingCodes/EditModal.cshtml.cs
@@ -3,8 +3,12 @@ using Dolphin.Freight.AccountingSettings.GlCodes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form;
 using Volo.Abp.ObjectMapping;
+using SelectItems = Microsoft.AspNetCore.Mvc.Rendering.SelectListItem;
 
 namespace Dolphin.Freight.Web.Pages.AccountingSettings.BillingCodes
 {
@@ -17,13 +21,19 @@ namespace Dolphin.Freight.Web.Pages.AccountingSettings.BillingCodes
         public CreateUpdateBillingCodeDto BillingCode { get; set; }
         private readonly IBillingCodeAppService _billingCodeAppService;
 
+        private readonly IGlCodeAppService _glCodeAppService;
+        [BindProperty]
+        public List<SelectItems> GiCodes { get; set; }
+
+
         public EditModalModel(IBillingCodeAppService billingCodeAppService)
         {
             _billingCodeAppService = billingCodeAppService;
-
+            GiCodes = new List<SelectItems>();
         }
         public async Task OnGetAsync()
         {
+            await GetGiCodesAsync();
             var billingCode = await _billingCodeAppService.GetAsync(Id);
             BillingCode = ObjectMapper.Map < BillingCodeDto, CreateUpdateBillingCodeDto >(billingCode);
         }
@@ -31,6 +41,16 @@ namespace Dolphin.Freight.Web.Pages.AccountingSettings.BillingCodes
         {
             await _billingCodeAppService.UpdateAsync(Id, BillingCode);
             return NoContent();
+        }
+
+        private async Task GetGiCodesAsync()
+        {
+            var glCodes = (await _glCodeAppService.GetGlCodesAsync(new Common.QueryDto())).Select(x => new SelectItems
+            {
+                Text = x.Code,
+                Value = x.Id.ToString()
+            }).ToList();
+            GiCodes.AddRange(glCodes);
         }
     }
 }
