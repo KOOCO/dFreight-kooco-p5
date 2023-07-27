79d6ee640c1d42e6d9340b2e5aa641459aaf280b
Pradeep <PrdpShrma13@users.noreply.github.com>
2023-07-28 00:08:21 +0530

    Commit For DFREIGHT-996 (Settings-Package Unit Manager-Package Code)

--- src/Dolphin.Freight.Web/Pages/Settings/PackageUnits/CreateModal.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/Settings/PackageUnits/CreateModal.cshtml.cs
@@ -1,9 +1,9 @@
-using Dolphin.Freight.Settings.AwbNoRanges;
-using Dolphin.Freight.Settings.PackageUnits;
+using Dolphin.Freight.Localization;
 using Dolphin.Freight.Settinngs.PackageUnits;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
 using System.Threading.Tasks;
+using Volo.Abp;
 
 namespace Dolphin.Freight.Web.Pages.Settings.PackageUnits
 {
@@ -12,17 +12,21 @@ namespace Dolphin.Freight.Web.Pages.Settings.PackageUnits
         [BindProperty]
         public CreateUpdatePackageUnitDto PackageUnit { get; set; }
         private readonly IPackageUnitAppService _packageUnitAppService;
+        private readonly IStringLocalizer<FreightResource> _localizer;
 
-        public CreateModalModel(IPackageUnitAppService packageUnitAppService)
+        public CreateModalModel(IPackageUnitAppService packageUnitAppService, IStringLocalizer<FreightResource> localizer)
         {
             _packageUnitAppService = packageUnitAppService;
-
+            _localizer = localizer;
         }
         public void OnGet()
         {
         }
         public async Task<IActionResult> OnPostAsync()
         {
+            bool isPackageUnitAlreadyExist = await _packageUnitAppService.IsPackageUnitAlreadyExist(PackageUnit.PackageCode);
+            if (isPackageUnitAlreadyExist)
+                throw new UserFriendlyException(string.Format(_localizer["PackageUnitAlreadyExist"].ToString(), PackageUnit.PackageCode));
             await _packageUnitAppService.CreateAsync(PackageUnit);
             return NoContent();
         }
