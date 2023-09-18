c0f06810cc58056798c587c5c389519e82aacf57
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-09-18 19:01:46 +0530

    DFreight-236 Partially Completed.

--- src/Dolphin.Freight.Web/Pages/OceanImports/EditModel.js
+++ src/Dolphin.Freight.Web/Pages/OceanImports/EditModel.js
@@ -1,12 +1,10 @@
 ﻿$(function () {
-    debugger;
     var url = new URL(window.location.href);
 
     dolphin.freight.importExport.oceanImports.oceanImportHbl.getHblCardsById(url.searchParams.get('Id'))
            .done(function (hblCards) {
                 if (hblCards && hblCards.length) {
                     hblCards.forEach(function (hblCard, index) {
-
 
                         let abpcard = createHawbCard();
 
@@ -16,7 +14,12 @@
 
                     })
                     setTimeout(() => {
-                        $('.hblCardTitle')[0].click();
+                        var from = url.searchParams.get('from');
+                        if (from === 'NewHbl') {
+                            $('#addHBtn').click();
+                        } else {
+                            $('.hblCardTitle')[0].click();
+                        }
                     }, 500);
                 }
             })
--- src/Dolphin.Freight.Web/Pages/OceanImports/EditModel3.js
+++ src/Dolphin.Freight.Web/Pages/OceanImports/EditModel3.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     var url = new URL(window.location.href);
 
-    dolphin.freight.importExport.airImports.airImportHawb.getHblCardsById(url.searchParams.get('Id'))
+    dolphin.freight.importExport.oceanImports.oceanImportHbl.getHblCardsById(url.searchParams.get('Id'))
         .done(function (hblCards) {
             if (hblCards && hblCards.length) {
                 hblCards.forEach(function (hblCard, index) {
@@ -10,13 +10,11 @@
 
                     abpcard = setHawbCardValues(abpcard, hblCard.id, hblCard.hblNo, index);
 
-                    $('#hblCards').append(abpcard);
-
+                    $('#hblCardDiv').append(abpcard);
                 })
                 setTimeout(() => {
                     $('.hblCardTitle')[0].click();
                 }, 500);
             }
-        })
-
-})
+        });
+});
