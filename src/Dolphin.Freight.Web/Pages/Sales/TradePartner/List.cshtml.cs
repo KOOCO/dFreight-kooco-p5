0cdcaf704f158bf640d6bbed3ecf0053b2a977ea
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2023-09-21 10:36:35 +0500

    Commit For DFREIGHT-857

--- src/Dolphin.Freight.Web/Pages/Sales/TradePartner/List.cshtml.cs
+++ src/Dolphin.Freight.Web/Pages/Sales/TradePartner/List.cshtml.cs
@@ -1,13 +1,34 @@
+using Dolphin.Freight.TradePartners;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Dolphin.Freight.Web.Pages.Sales.TradePartner
 {
     public class ListModel : PageModel
     {
+        public List<SelectListItem> AccountGroupNameLookupList { get; set; }
+        public List<SelectListItem> CountryLookupList { get; set; }
+        private readonly ITradePartnerAppService _tradePartnerAppService;
+        private readonly IAccountGroupAppService _accountGroupAppService;
         public string Search { get; set; }
-        public void OnGet()
+        public ListModel(IAccountGroupAppService accountGroupAppService, ITradePartnerAppService tradePartnerAppService) { 
+        _accountGroupAppService= accountGroupAppService;
+            _tradePartnerAppService= tradePartnerAppService;
+        }
+        public async Task OnGetAsync()
         {
+            var accountGroupNameLookup = await _accountGroupAppService.GetAccountGroupNameLookupAsync();
+            AccountGroupNameLookupList = accountGroupNameLookup.Items
+                                                .Select(x => new SelectListItem(x.AccountGroupName, x.Id.ToString(), false))
+                                                .ToList<SelectListItem>();
+            var countryNameLookup = await _tradePartnerAppService.GetCountriesLookupAsync();
+            CountryLookupList = countryNameLookup.Items
+                                                .Select(x => new SelectListItem(x.ShowName + " " + x.CountryName, x.Id.ToString(), false))
+                                                .ToList();
         }
     }
 }
