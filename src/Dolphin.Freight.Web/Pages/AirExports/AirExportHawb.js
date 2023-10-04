db2b0b038b8adc4c0867b5393504d2e8ecf4c74a
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-10-04 10:50:37 +0530

    DFreight-1224 Formula Fixed.

--- src/Dolphin.Freight.Web/Pages/AirExports/AirExportHawb.js
+++ src/Dolphin.Freight.Web/Pages/AirExports/AirExportHawb.js
@@ -2,11 +2,11 @@
     static KgLbConversion(Elem, Unit) {
         switch (Unit) {
             case 'KG':
-                var LB = parseFloat(Elem.currentTarget.value * 2.2).toFixed(2);
+                var LB = parseFloat(Elem.currentTarget.value * 2.20462062).toFixed(2);
                 $($(Elem.currentTarget).parent().parent().next().children().children()[0]).val(LB);
                 break;
             case 'LB':
-                var KG = parseFloat(Elem.currentTarget.value / 2.2).toFixed(2);
+                var KG = parseFloat(Elem.currentTarget.value / 2.20462062).toFixed(2);
                 $($(Elem.currentTarget).parent().parent().prev().children().children()[0]).val(KG);
                 break;
         }
