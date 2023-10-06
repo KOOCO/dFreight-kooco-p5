6a859383453113cb60f7de892027351d722c286c
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2023-10-06 11:30:51 +0500

    fix for 167

--- src/Dolphin.Freight.Application.Contracts/ImportExport/AirExports/Bookings/CreateUpdateExportBookingDto.cs
+++ src/Dolphin.Freight.Application.Contracts/ImportExport/AirExports/Bookings/CreateUpdateExportBookingDto.cs
@@ -143,7 +143,7 @@ namespace Dolphin.Freight.ImportExport.AirExports.Bookings
         /// 收貨地(POR)ID
         /// </summary>
 
-        public DateTime FinalEta { get; set; }
+        public DateTime? FinalEta { get; set; }
 
         /// <summary>
         /// 中轉港 ID
@@ -224,7 +224,7 @@ namespace Dolphin.Freight.ImportExport.AirExports.Bookings
         /// 出發日期
         /// </summary>
         [DataType(DataType.Date)]
-        public DateTime DepatureDate { get; set; }
+        public DateTime? DepatureDate { get; set; }
         /// <summary>
         /// 航班號碼
         /// </summary>
