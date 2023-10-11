0ae51dc5249424bc89597533bbc1f521cbfcfb1f
Mujahid Ali Kayani <98872876+MujahidAliKiani@users.noreply.github.com>
2023-10-11 11:16:47 +0500

    Commit For DFREIGHT-927

--- src/Dolphin.Freight.Web/Pages/Accounting/Payment/PaymentMadeList/Index.js
+++ src/Dolphin.Freight.Web/Pages/Accounting/Payment/PaymentMadeList/Index.js
@@ -1,145 +1,113 @@
-﻿$(function () {
-    var l = abp.localization.getResource('Freight');
+﻿var l = abp.localization.getResource('Freight');
+var columns = [
 
 
-    var dataTable = $('#PaymentMadeListTable').DataTable(
-        abp.libs.datatables.normalizeConfiguration({
-            serverSide: true,
-            paging: true,
-            order: [[2, "asc"]],
-            searching: true,
-            scrollX: true,
-            responsive: {
-                details: {
-                    type: 'column'
-                }
-            },
-            ajax: abp.libs.datatables.createAjax(dolphin.freight.accounting.payment.payment.getDataList, function () {
-                return {};
-            }),
-            columnDefs: [
-                {
-                    className: 'dtr-control',
-                    orderable: false,
-                    "defaultContent": ""
-                },
-                        {
-                            title: l('Actions'),
-                            rowAction: {
-                                items:
-                                    [
-                                        {
-                                            text: l('Edit'),
-                                            visible: abp.auth.isGranted('Accounting.PaymentMadeList.Edit'), //CHECK for the PERMISSION
-                                            action: function (data) {
-                                                if (data.record.isLocked) {
+    {
+        title: l('Actions'),
+        rowAction: {
+            items:
+                [
+                    {
+                        text: l('Edit'),
+                        visible: abp.auth.isGranted('Accounting.PaymentMadeList.Edit'), //CHECK for the PERMISSION
+                        action: function (data) {
+                            if (data.record.isLocked) {
 
-                                                }
-                                                location.href = '/Accounting/Payment/Payment?Id=' + data.record.id + '&edit=Y';
-
-                                            }
-                                        },
-                                        {
-                                            text: l('Delete'),
-                                            confirmMessage: function (data) {
-                                                return l(
-                                                    'AreYouSureToDelete'
-                                                );
-                                            },
-                                            action: function (data) {
-                                                dolphin.freight.accounting.payment.payment
-                                                    .delete(data.record.id)
-                                                    .then(function () {
-                                                        abp.notify.info(l('SuccessfullyDeleted'));
-                                                        dataTable.ajax.reload();
-                                                    });
-                                            }
-                                        }
-                                    ]
                             }
-                        },
-                        {
-                            data: "id",
-                            visible: false,
-                            searchable: false,
-                            type: "hidden"
-                        },
-                        {
-                            title: l('PaymentMadeList:ReleaseDate'),
-                            data: "releaseDate",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:PaidTo'),
-                            data: "paidToName",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:Category'),
-                            data: "category",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:CheckNo'),
-                            data: "checkNo",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:Bank'),
-                            data: "bank",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:PaidAmount') + '(' + "TWD" + ')',
-                            data: "bankCurrency",
-                            type: "readonly"
-                        },
-                     
-                        {
-                            title: l('PaymentMadeList:ClearDate'),
-                            data: "clear",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:Invalid'),
-                            data: "void",
-                            render: function (data, type, row) {
-                                if (data === true) {
-                                    return '<i class="fas fa-check"></i>'; // Use an icon for true (checkmark)
-                                } else {
-                                    return '<i class="fas fa-times"></i>'; // Use an icon for false (cross)
-                                }
-                            }
-                        },
-                        {
-                            title: l('PaymentMadeList:InvalidDate'),
-                            data: "invalid",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:Substation'),
-                            data: "officeName",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:CreatorId'),
-                            data: "creatorId",
-                            type: "readonly"
-                        },
-                        {
-                            title: l('PaymentMadeList:Memo'),
-                            data: "memo",
-                            type: "readonly"
+                            location.href = '/Accounting/Payment/Payment?Id=' + data.record.id + '&edit=Y';
+
                         }
-                        //,
-                        //{
-                        //    title: l('PaymentMadeList:Status'),
-                        //    data: "",
-                        //    type: "readonly"
-                        //}
-                    ]
-        })
-    );
+                    },
+                    {
+                        text: l('Delete'),
+                        confirmMessage: function (data) {
+                            return l(
+                                'AreYouSureToDelete'
+                            );
+                        },
+                        action: function (data) {
+                            dolphin.freight.accounting.payment.payment
+                                .delete(data.record.id)
+                                .then(function () {
+                                    abp.notify.info(l('SuccessfullyDeleted'));
+                                    dataTable.ajax.reload();
+                                });
+                        }
+                    }
+                ]
+        }
+    }
+]
+$(function () {
+  
+    dolphin.freight.web.controllers.configuration.getJsonConfig('PaymentMadeList').done(function (data) {
+        data.forEach(function (item) {
+            if (!item.lock && item.checkable) {
+                var column;
+
+
+                if (item.text.toLowerCase() == 'paymentmadelist:invalid') {
+                    column = {
+                        //是否鎖定
+                        title: l('PaymentMadeList:Invalid'),
+                        orderable: false,
+                        render: function (data, type, row, meta) {
+                            if (row.void === true) {
+                                return '<i class="fas fa-check"></i>'; // Use an icon for true (checkmark)
+                            } else {
+                                return '<i class="fas fa-times"></i>'; // Use an icon for false (cross)
+                            }
+                        }
+                    }
+                }
+                else {
+                    column = {
+                        title: l(item.text),
+                        data: item.name,
+                        render: function (data, type, row) {
+                            return data === null ? " " : data;
+                        }
+                    };
+                }
+                columns.push(column);
+            }
+        });
+
+        var col = (columns.length > 2) ? [[2, 'asc']] : [[0, 'asc']];
+
+        dataTable = $('#PaymentMadeListTable').DataTable(
+            abp.libs.datatables.normalizeConfiguration({
+                serverSide: true,
+                paging: true,
+                order: col,
+                searching: false,
+                scrollX: true,
+                processing: true,
+                responsive: {
+                    details: {
+                        type: 'column'
+                    }
+                },
+                ajax: abp.libs.datatables.createAjax(dolphin.freight.accounting.payment.payment.getDataList, function () {
+                    return {};
+                }),
+                columnDefs: columns
+            })
+        );
+
+       
+    });
+
+    $('#btnConfiguration').click(function (e) {
+        var _configurationModal = new abp.ModalManager({
+            viewUrl: abp.appPath + 'Configuration',
+            modalClass: 'ConfigurationViewModel'
+        });
+
+        _configurationModal.open({
+            src: 'PaymentMadeList'
+        });
+    })
 
     function FIndTpName(id)
     {
