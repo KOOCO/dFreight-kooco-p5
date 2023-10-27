a78867773a6f2043fd5537ef5628a81a3e37ba6a
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-10-27 14:03:41 +0530

    FK-Migrations Done For New DB.

---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20231027072329_FK_Constraint_AppPortsManagement_AppVesselSchedules.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class FK_Constraint_AppPortsManagement_AppVesselSchedules : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_DelId",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_DelId",
+                table: "AppVesselSchedules",
+                column: "DelId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_FdestId",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_FdestId",
+                table: "AppVesselSchedules",
+                column: "FdestId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_PodId",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_PodId",
+                table: "AppVesselSchedules",
+                column: "PodId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_PolId",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_PolId",
+                table: "AppVesselSchedules",
+                column: "PolId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_PorId",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_PorId",
+                table: "AppVesselSchedules",
+                column: "PorId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppVesselSchedules_AppPorts_TransPort1Id",
+                table: "AppVesselSchedules"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppVesselSchedules_AppPortsManagements_TransPort1Id",
+                table: "AppVesselSchedules",
+                column: "TransPort1Id",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20231027081525_FK_Constraints_AppPortsManagement_AppAirImportMawbs.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class FK_Constraints_AppPortsManagement_AppAirImportMawbs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppAirImportMawbs_AppAirports_DepatureId",
+                table: "AppAirImportMawbs"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppAirImportMawbs_AppPortsManagements_DepatureId",
+                table: "AppAirImportMawbs",
+                column: "DepatureId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppAirImportMawbs_AppAirports_DestinationId",
+                table: "AppAirImportMawbs"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppAirImportMawbs_AppPortsManagements_DestinationId",
+                table: "AppAirImportMawbs",
+                column: "DestinationId",
+                principalTable: "AppPortsManagements",
+                principalColumn: "Id"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
