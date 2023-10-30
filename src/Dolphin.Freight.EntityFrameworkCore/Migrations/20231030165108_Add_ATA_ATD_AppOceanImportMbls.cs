3d622ed67fc3e207283718a8e0d9259848fbb58e
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-10-30 22:51:22 +0530

    DFreight-1303 OceanImport MBL - DONE.

---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20231030165108_Add_ATA_ATD_AppOceanImportMbls.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class Add_ATA_ATD_AppOceanImportMbls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ATA",
+                table: "AppOceanImportMbls",
+                type: "datetime2",
+                nullable: true
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ATD",
+                table: "AppOceanImportMbls",
+                type: "datetime2",
+                nullable: true
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ATA",
+                table: "AppOceanImportMbls"
+            );
+
+            migrationBuilder.DropColumn(
+                name: "ATD",
+                table: "AppOceanImportMbls"
+            );
+        }
+    }
+}
