0a2ea896943e754474c3b329286a5ace818860e7
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-11-28 17:29:52 +0530

    DFreight-1365.

---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20231128103301_Change_AppSysCodes_BillType_ShowName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class Change_AppSysCodes_BillType_ShowName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE AppSysCodes SET IsDeleted = '1' WHERE CodeType = 'BillType' AND IsDeleted = '0'");
+            migrationBuilder.Sql("INSERT INTO AppSysCodes (Id, CodeType, CodeValue, ShowName, IsDeleted, CreationTime) VALUES (NEWID(), 'BillType', 'OurCost', 'OurCost', '0', GETDATE())");
+            migrationBuilder.Sql("INSERT INTO AppSysCodes (Id, CodeType, CodeValue, ShowName, IsDeleted, CreationTime) VALUES (NEWID(), 'BillType', 'PayforAgent', 'PayforAgent', '0', GETDATE())");
+            migrationBuilder.Sql("INSERT INTO AppSysCodes (Id, CodeType, CodeValue, ShowName, IsDeleted, CreationTime) VALUES (NEWID(), 'BillType', 'CostforAgent', 'CostforAgent', '0', GETDATE())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
