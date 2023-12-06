b9fd67f5038a2e0b5f4985f513c994e2d43a09ef
Veer Mevada <veer.mevada@zobiwebsolutions.com>
2023-12-06 13:20:56 +0530

    DFreight-1396, 1395, 1392 Fixed.

---
+++ src/Dolphin.Freight.EntityFrameworkCore/Migrations/20231206072904_Change_AppOceanImportHbls_DeliveryToId_FK_Constraint_TO_AppTradePartners.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dolphin.Freight.Migrations
+{
+    public partial class Change_AppOceanImportHbls_DeliveryToId_FK_Constraint_TO_AppTradePartners : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE AppOceanImportHbls SET DeliveryToId = NULL WHERE DeliveryToId is not NULL");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppOceanImportHbls_AppPortsManagements_DeliveryToId",
+                table: "AppOceanImportHbls"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppOceanImportHbls_AppTradePartners_DeliveryToId",
+                table: "AppOceanImportHbls",
+                column: "DeliveryToId",
+                principalTable: "AppTradePartners",
+                principalColumn: "Id"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
